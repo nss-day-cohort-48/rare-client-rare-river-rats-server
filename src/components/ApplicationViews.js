adb433d4a716e8ca1ee163adb81c66bebf9765a7
rbgreenway3rd <3rdgreenway@gmail.com>
2021-07-29 11:42:29 -0500

    began updating provider, started on Posts

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,21 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+// import { Route } from "react-router-dom";
+// import { ProfileProvider } from "./auth/AuthProvider";
+// import { RareUserProvider } from "./RareUser/RareUserProvider";
+// import { CategoryProvier } from "./categories/CategoryProvier";
+// import { CommentProvider } from "./comments/CommentProvider";
+// import { TagProvider } from "./tags/TagProvider";
+// import { PostProvider } from "./posts/PostProvider";
 
 export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
-            lineHeight: "1.75rem"
-        }}>
-        </main>
+  return (
+    <>
+      <main
+        style={{
+          margin: "5rem 2rem",
+          lineHeight: "1.75rem",
+        }}
+      ></main>
     </>
-}
+  );
+};
---
+++ src/components/posts/Post.js
@@ -0,0 +1,3 @@
+import React, { useState } from "react";
+
+export const PostContext = React.createContext();
