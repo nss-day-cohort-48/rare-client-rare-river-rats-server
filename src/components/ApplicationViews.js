e0b24c2d8e2cdd8b4ece206d33d8ec95fe1ecd63
keyanoush <keyanoush@Keyanoush.localdomain>
2021-07-29 15:46:40 -0500

    category form

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,8 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { Category } from "./categories/Category"
+import { CategoryForm } from "./categories/CategoryForm"
+import { CategoryProvider } from "./categories/CategoryProvider"
 
 export const ApplicationViews = () => {
     return <>
@@ -7,6 +10,14 @@ export const ApplicationViews = () => {
             margin: "5rem 2rem",
             lineHeight: "1.75rem"
         }}>
+            <CategoryProvider>
+                <Route exact path="/categories">
+                    <Category />
+                </Route>
+                <Route exact path="/categories/create">
+                    <CategoryForm />
+                </Route>
+            </CategoryProvider>
         </main>
     </>
 }
