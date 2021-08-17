106488c721a93027646043a26904e1893337884d
rbgreenway3rd <3rdgreenway@gmail.com>
2021-08-17 15:30:28 -0500

    Corrected faulty code caused by merge mistake in Login.js

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,17 +2,14 @@ import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import "./Auth.css";
 
-export const Login = (props) => {
-  const history = useHistory();
+export const Login = () => {
   const email = React.createRef();
   const password = React.createRef();
   const invalidDialog = React.createRef();
+  const history = useHistory();
 
-  export const Login = () => {
-    const email = React.createRef();
-    const password = React.createRef();
-    const invalidDialog = React.createRef();
-    const history = useHistory();
+  const handleLogin = (e) => {
+    e.preventDefault();
 
     return fetch("http://127.0.0.1:8000/login", {
       method: "POST",
@@ -29,89 +26,68 @@ export const Login = (props) => {
       .then((res) => {
         if ("valid" in res && res.valid && "token" in res) {
           localStorage.setItem("rare_token", res.token);
-          props.history.push("/");
+          history.push("/");
         } else {
           invalidDialog.current.showModal();
         }
       });
   };
 
-  return fetch("http://127.0.0.1:8000/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      username: email.current.value,
-      password: password.current.value,
-    }),
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      if ("valid" in res && res.valid && "token" in res) {
-        localStorage.setItem("rare_token", res.token);
-        history.push("/");
-      } else {
-        invalidDialog.current.showModal();
-      }
-    });
-};
-
-return (
-  <main className="container--login">
-    <dialog className="dialog dialog--auth" ref={invalidDialog}>
-      <div>Email or password was not valid.</div>
-      <button
-        className="button--close"
-        onClick={(e) => invalidDialog.current.close()}
-      >
-        Close
-      </button>
-    </dialog>
-    <section>
-      <form className="form--login" onSubmit={handleLogin}>
-        <h1>Rare Publishing</h1>
-        <h2>Please sign in</h2>
-        <fieldset>
-          <label htmlFor="inputEmail"> Email address </label>
-          <input
-            ref={email}
-            type="email"
-            id="email"
-            className="form-control"
-            placeholder="Email address"
-            required
-            autoFocus
-          />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="inputPassword"> Password </label>
-          <input
-            ref={password}
-            type="password"
-            id="password"
-            className="form-control"
-            placeholder="Password"
-            required
-          />
-        </fieldset>
-        <fieldset
-          style={{
-            textAlign: "center",
-          }}
+  return (
+    <main className="container--login">
+      <dialog className="dialog dialog--auth" ref={invalidDialog}>
+        <div>Email or password was not valid.</div>
+        <button
+          className="button--close"
+          onClick={(e) => invalidDialog.current.close()}
         >
-          <button className="btn btn-1 btn-sep icon-send" type="submit">
-            Sign In
-          </button>
-        </fieldset>
-      </form>
-    </section>
-    <section className="link--register">
-      <Link to="/register">Not a member yet?</Link>
-    </section>
-  </main>
-);
+          Close
+        </button>
+      </dialog>
+      <section>
+        <form className="form--login" onSubmit={handleLogin}>
+          <h1>Rare Publishing</h1>
+          <h2>Please sign in</h2>
+          <fieldset>
+            <label htmlFor="inputEmail"> Email address </label>
+            <input
+              ref={email}
+              type="email"
+              id="email"
+              className="form-control"
+              placeholder="Email address"
+              required
+              autoFocus
+            />
+          </fieldset>
+          <fieldset>
+            <label htmlFor="inputPassword"> Password </label>
+            <input
+              ref={password}
+              type="password"
+              id="password"
+              className="form-control"
+              placeholder="Password"
+              required
+            />
+          </fieldset>
+          <fieldset
+            style={{
+              textAlign: "center",
+            }}
+          >
+            <button className="btn btn-1 btn-sep icon-send" type="submit">
+              Sign In
+            </button>
+          </fieldset>
+        </form>
+      </section>
+      <section className="link--register">
+        <Link to="/register">Not a member yet?</Link>
+      </section>
+    </main>
+  );
+};
 
 // import React, { useRef } from "react";
 // import { Link, useHistory } from "react-router-dom";
