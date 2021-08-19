a2df7ef7c4de3f7c374489783c56093448069b87
rbgreenway3rd <3rdgreenway@gmail.com>
2021-08-19 09:36:24 -0500

    housekeeping. Needing to push for team.

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,28 +1,27 @@
-import React, {useContext, useEffect, useState} from "react";
-import { PostContext } from "./PostProvider"
-import "./Posts.css";
-import { useHistory, useParams  } from "react-router";
+// import React, {useContext, useEffect, useState} from "react";
+// import { PostContext } from "./PostProvider"
+// import "./Posts.css";
+// import { useHistory, useParams  } from "react-router";
 
-export const Post = () => {
-  const { posts, getPosts } = useContext(PostContext)
-  const [ post, setPost ] = useState({})
+// export const Post = () => {
+//   const { posts, getPosts } = useContext(PostContext)
+//   const [ post, setPost ] = useState({})
 
-  const { postId } = useParams();
+//   const { postId } = useParams();
 
-  const history = useHistory();
+//   const history = useHistory();
 
+//   useEffect(() => {
+//     getPosts()
+//   }, [])
 
-  useEffect(() => {
-    getPosts()
-  }, [])
-
-  useEffect(() => {
-    const 
-  })
-  <section className="post">
-    <h3 className="post__title">
-      <div>{post.title}</div>
-    </h3>
-    <div className="post__content">{post.content}</div>
-  </section>
-};
+//   useEffect(() => {
+//     const
+//   })
+//   <section className="post">
+//     <h3 className="post__title">
+//       <div>{post.title}</div>
+//     </h3>
+//     <div className="post__content">{post.content}</div>
+//   </section>
+// };
